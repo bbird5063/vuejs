9e5ed367f3e0d11bde89f8cf923af348e791d27d
bbird5063 <bbird5063@gmail.com>
2023-08-18 14:50:07 +0300

    01:23:20 ➝ Индикатор загрузки данных

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-    <!--my-button @click="fetchPost">Получить посты</my-button-->
     <my-button
       @click="showDialog"
       style="margin: 15px 0;"
@@ -11,11 +10,13 @@
     <my-dialog v-model:show="dialodVisible">
       <post-form @create="createPost" />
     </my-dialog>
-
+    <!-- #3 -->
     <post-list
       :posts="posts"
       @remove="removePost"
+      v-if="!isPostsLoading"
     />
+    <div v-else>Идет загрузка...</div>
   </div>
 </template>
 
@@ -30,9 +31,9 @@ export default {
   },
   data() {
     return {
-      posts: [
-      ],
+      posts: [],
       dialodVisible: false, // создали директиву
+      isPostsLoading: false // #2 модель для индикации загрузки
     }
   },
 
@@ -50,10 +51,16 @@ export default {
 
     async fetchPost() {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
-        this.posts = response.data;
+        this.isPostsLoading = true; // перед отправкой запроса
+        setTimeout(async () => { // #1
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+          this.posts = response.data;
+          this.isPostsLoading = false; // пока используем setTimeout (пока тестовый режим)
+        }, 2000);
       } catch (e) {
         alert('Ошибка');
+      } finally {
+        //this.isPostsLoading = false; // При работе здесь (пока тестовый режим)
       }
     }
   },
