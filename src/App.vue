9048ba3f48d684996e19e56fc06b3b3ebc728049
bbird5063 <bbird5063@gmail.com>
2023-08-21 01:55:41 +0300

    01:39:08 ➝ Анимации transition group (рабочая сортировка)

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,23 @@
   <div class="app">
     <h1>Страница с постами</h1>
     <div class="app_btns">
-      <my-button @click="showDialog">
-        Создать пост
-      </my-button>
+      <my-button @click="showDialog"> Создать пост </my-button>
       <!-- 
         1. v-model="selectedSort".  v-model:value = selectedSort - через $emit
         2. :options="sortOption". байдим options - пропс массив, который ждет MySelect (по нему будет v-for). sortOption - массив, который определяем здесь
        -->
       <my-select v-model="selectedSort" :options="sortOption" />
-
     </div>
 
     <my-dialog v-model:show="dialodVisible">
       <post-form @create="createPost" />
     </my-dialog>
 
-    <post-list :posts="sortedPosts" @remove="removePost" v-if="!isPostsLoading" />
+    <post-list
+      :posts="sortedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
     <div v-else>Идет загрузка...</div>
   </div>
 </template>
@@ -25,23 +26,25 @@
 <script>
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   components: {
-    PostForm, PostList
+    PostForm,
+    PostList,
   },
   data() {
     return {
       posts: [],
       dialodVisible: false,
       isPostsLoading: false,
-      selectedSort: '',
-      sortOption: [ // массив option для нашего select
-        { value: 'title', name: 'По названию' },
-        { value: 'body', name: 'По содержанию' },
-      ]
-    }
+      selectedSort: "",
+      sortOption: [
+        // массив option для нашего select
+        { value: "title", name: "По названию" },
+        { value: "body", name: "По содержанию" },
+      ],
+    };
   },
 
   methods: {
@@ -50,7 +53,7 @@ export default {
       this.dialodVisible = false;
     },
     removePost(post) {
-      this.posts = this.posts.filter(p => p.id !== post.id);
+      this.posts = this.posts.filter((p) => p.id !== post.id);
     },
     showDialog() {
       this.dialodVisible = true;
@@ -60,30 +63,35 @@ export default {
       try {
         this.isPostsLoading = true;
         //setTimeout(async () => { // #1
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+        );
         this.posts = response.data;
         //this.isPostsLoading = false; // пока используем setTimeout (пока тестовый режим)
         //}, 2000);
       } catch (e) {
-        alert('Ошибка');
+        alert("Ошибка");
       } finally {
         this.isPostsLoading = false; // При работе здесь (пока тестовый режим)
       }
-    }
+    },
   },
 
   mounted() {
-    this.fetchPost()
+    this.fetchPost();
   },
 
   computed: {
     sortedPosts() {
+      console.l;
       return [...this.posts].sort((post1, post2) => {
-        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
-      })
+        return post1[this.selectedSort]?.localeCompare(
+          post2[this.selectedSort]
+        );
+      });
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,33 +1,48 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
-    <post-item
-      v-for="post in posts"
-      :post="post"
-      :key="post.id"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
 
-  <h2
-    v-else
-    style="color:red"
-  >
-    Список пользователей пуст
-  </h2>
+  <h2 v-else style="color: red">Список пользователей пуст</h2>
 </template>
- 
+
 <script>
 import PostItem from "@/components/PostItem";
 export default {
   components: { PostItem },
   props: {
-    posts: { // пропс posts пришел от родителя App
+    posts: {
+      // пропс posts пришел от родителя App
       type: Array,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
